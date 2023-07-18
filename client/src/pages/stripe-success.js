40a5550d2da2439ed302bc78b668e8a2439dd24a
Israel Sepulveda <sepulveda.israel@gmail.com>
2023-07-18 13:19:41 -0400

    Initial commit

---
+++ client/src/pages/stripe-success.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect, useContext } from 'react'
+import axios from 'axios'
+import { SyncOutlined } from "@ant-design/icons"
+import { UserContext } from '../context'
+
+const StripeSucess = ({ history }) => {
+    const [state, setState ] = useContext(UserContext)
+    useEffect(() => {
+        const getSubscriptionStatus = async () => {
+            const {data} = await axios.get('/subscription-status')
+                console.log('Subsciption status => ', data)
+                if(data && data.length === 0){
+                history.pushState('/')
+            }else{
+                //update user in local storage
+                const auth = JSON.parse(localStorage.getItem("auth"))
+                auth.user = data 
+                localStorage.setItem('auth', JSON.stringify(auth))
+                // update user in context
+                setState(auth)
+                setTimeout(() => {
+                    history.push("/account")
+                }, 1000)
+            }}
+            getSubscriptionStatus()
+    }, [])
+    return (
+        <div className='d-flex justify-content-center fw-bold' style={{ height: '90vh'}}>
+            <div className='d-flex align-items-center'>
+                <SyncOutlined spin style={{ fontSize: '50px'}} />
+            </div>
+        </div>
+    )
+}
+
+export default StripeSucess
